af10843e2fe829842ee4e59fcac48da30e052688
a.simanov <nemodev@yandex.ru>
2024-07-10 18:23:07 +0300

    Доработан build-local.sh

--- build-local.sh
+++ build-local.sh
@@ -5,17 +5,17 @@
 ./gradlew :core:extensions:build :core:extensions:publishToMavenLocal &&
 ./gradlew :core:validation:build :core:validation:publishToMavenLocal &&
 ./gradlew :core:metrics:build :core:metrics:publishToMavenLocal &&
+./gradlew :core:logging:build :core:logging:publishToMavenLocal &&
 ./gradlew :core:logging-sl4j:build :core:logging-sl4j:publishToMavenLocal &&
 ./gradlew :core:build-info:build :core:build-info:publishToMavenLocal &&
-./gradlew :core:tracing:build :core:tracing:publishToMavenLocal &&
 ./gradlew :core:api:build :core:api:publishToMavenLocal &&
+./gradlew :core:tracing:build :core:tracing:publishToMavenLocal &&
 ./gradlew :core:open-api:build :core:open-api:publishToMavenLocal &&
 ./gradlew :core:exception:build :core:exception:publishToMavenLocal &&
 ./gradlew :core:async:build :core:async:publishToMavenLocal &&
 ./gradlew :core:api-exception-handler:build :core:api-exception-handler:publishToMavenLocal &&
-./gradlew :core:logging:build :core:logging:publishToMavenLocal &&
 ./gradlew :core:http-server:build :core:http-server:publishToMavenLocal &&
-./gradlew :starter:core:build :starter:core:publishToMavenLocal
+./gradlew :core:starter:build :core:starter:publishToMavenLocal
 ./gradlew :db:core:build :db:core:publishToMavenLocal &&
 ./gradlew :integration:http:build :integration:http:publishToMavenLocal &&
 ./gradlew :integration:kafka:build :integration:kafka:publishToMavenLocal &&
